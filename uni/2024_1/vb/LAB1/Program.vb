56ff5236a54e6176fd188294f96d2e6ee44483fb
Roko <rokovukovic191@gmail.com>
2024-10-17 09:49:41 +0200

    First commit; Kod za LAB1 zad 5 i 6 treba napisat

---
+++ uni/2024_1/vb/LAB1/Program.vb
@@ -0,0 +1,84 @@
+' Option Explicit Off ' OVO OPCIJU KORISTITI SAMO KAD SE TREBA POKRENUTI ZADATAK 4
+
+Imports System
+
+Module Program
+
+    ' Kod za ZADATAK 1
+    Sub miliToCentiAndMeter()
+        Dim mili as Double
+
+        Console.WriteLine("Napisi broj u milimetrima")
+        mili = Console.ReadLine()
+
+        Console.WriteLine($"{mili}mm je {mili / 10}cm i {mili / 1000}m")
+    End Sub
+
+    ' Kod za ZADATAK 2
+    Sub pow()
+        Dim base as Double
+        Dim expo as Double
+
+        Console.WriteLine("Upisi bazu: ")
+        base = Console.ReadLine()
+        Console.WriteLine("Upisi eksponent: ")
+        expo = Console.ReadLine()
+
+        Console.WriteLine($"Baza {base} na potenciju {expo} iznsoi {Math.pow(base, expo)}")
+
+    End Sub
+
+    ' Kod za ZADATAK 3
+    Sub VariableSwitch()
+        Dim broj1 as Integer
+        Dim broj2 as Integer
+
+        Console.WriteLine("Unesi prvi broj: ")
+        broj1 = Console.ReadLine()
+        Console.WriteLine("") ' Stvranje praznog reda za ljepsi izgled
+
+        Console.WriteLine("Unesi drugi broj: ")
+        broj2 = Console.ReadLine()
+        Console.WriteLine("")
+
+        Console.WriteLine("Stanje u varijablama prije switcha: ")
+        Console.WriteLine($"    broj1: {broj1}")
+        Console.WriteLine($"    broj2: {broj2}")
+        Console.WriteLine("")
+
+        ' Kod za switch varibli
+        broj1 += broj2
+        broj2 = broj1 - broj2
+        broj1 = broj1 - broj2
+
+        Console.WriteLine("Stanje u varijablama prije switcha: ")
+        Console.WriteLine($"    broj1: {broj1}")
+        Console.WriteLine($"    broj2: {broj2}")
+
+    End Sub
+
+    ' Kod za ZADATAK 4
+    Sub sqrt()
+
+        Console.WriteLine("Unesi broj koji zelis korjenovat: ")
+        brojZaKorijen = Console.ReadLine()
+        Console.WriteLine("")
+
+        Console.WriteLine($"Korijen broja {brojZaKorijen} je {Math.sqrt(brojZaKorijen)}")
+
+    End Sub
+
+    Sub radijus()
+    End Sub
+
+    Sub kibiMebiGibi()
+    End Sub
+
+    Sub Main()
+        ' miliToCentiAndMeter()
+        ' pow()
+        ' VariableSwitch()
+        ' sqrt() 'Ako zelis pokrenit ovu funkcije obavezno odkomentirati prvu linuju
+
+    End Sub
+End Module
